ad5d2954a0a3ec8155611a72d66996075352a80c
dione24 <dioneniangaly@gmail.com>
2022-05-09 14:34:19 +0000

    check

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -1,10 +1,7 @@
 <?php
 ob_start();
-
-
-// Define database connection constants
 try {
-	$baseDeDonnee = new PDO('mysql:host=localhost;dbname=newsugu', 'root', '');
+	$baseDeDonnee = new PDO('mysql:host=localhost;dbname=cp1146011p43_sugu', 'cp1146011p43_sugu', 'Diblo@cisco2019');
 	$baseDeDonnee->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$baseDeDonnee->exec('SET NAMES utf8');
 } catch (Exception $e) {
