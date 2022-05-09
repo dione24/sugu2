91328823805e41226dd56611b0bb409947c02e8b
dione24 <dioneniangaly@gmail.com>
2022-05-09 14:33:37 +0000

    ignore

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -1,5 +1,8 @@
 <?php
 ob_start();
+
+
+// Define database connection constants
 try {
 	$baseDeDonnee = new PDO('mysql:host=localhost;dbname=newsugu', 'root', '');
 	$baseDeDonnee->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
